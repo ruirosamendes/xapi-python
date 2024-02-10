c7457e89ceb2cc09de11d242ae5f26f4d7304084
Rui Rosa Mendes <rui.rosa_mendes@siemens.com>
2024-02-10 18:40:17 +0000

    closed positions

--- xtb_trading/get-closed-positions.py
+++ xtb_trading/get-closed-positions.py
@@ -20,7 +20,7 @@ async def get_symbol_data(socket: Socket, symbol:str):
     response = await socket.getSymbol(symbol)
     if response['status'] is True:
         data = pd.json_normalize(response['returnData'])
-        symbolData = data[["symbol","bid","ask",]]
+        symbolData = data[["symbol", "description", "categoryName", "currency", "bid","ask", "time"]]
     else:
         symbolData = None
         print("Failed to get symbol", response)
@@ -37,46 +37,47 @@ async def main():
                 closedTrades = data[["order", "symbol","volume","open_price","close_price","profit","open_time","close_time", "nominalValue","cmd"]]
                 closedTrades.insert(10, "opened_days", 0)
                 closedTrades["opened_days"] = round(((closedTrades["close_time"] - closedTrades["open_time"]) / 86400000), 1)
-                closedTrades.insert(11, "current_ask", 0.0)
-                closedTrades.insert(12, "current_bid", 0.0)
-                closedTrades.insert(13, "current_time", None)
-
+                closedTrades.insert(11, "description", "")
+                closedTrades.insert(12, "category", "")
+                closedTrades.insert(13, "currency", "")
+                closedTrades.insert(14, "current_ask", 0.0)
+                closedTrades.insert(15, "current_bid", 0.0)
+                closedTrades.insert(16, "current_time", 0)
+                
                 # Update current  bid, ask  and timeString
                 for index, row in closedTrades.iterrows():
                     symbol_data = await get_symbol_data(x.socket, row["symbol"])
+                    closedTrades.at[index, "description"] = symbol_data["description"].loc[0]
+                    closedTrades.at[index, "category"] = symbol_data["categoryName"].loc[0]
+                    closedTrades.at[index, "currency"] = symbol_data["currency"].loc[0]
                     closedTrades.at[index, "current_ask"] = round(symbol_data["ask"].loc[0],3)
                     closedTrades.at[index, "current_bid"] = round(symbol_data["bid"].loc[0],3)
+                    closedTrades.at[index, "current_time"] = symbol_data["time"].loc[0]
+                    
+                closedTrades.insert(17, "afterclosing_days", 0)           
+                closedTrades["afterclosing_days"] = round(((closedTrades["current_time"] - closedTrades["close_time"]) / 86400000), 1)
+                closedTrades.insert(18, "potential_profit", 0)
+                closedTrades["potential_profit"] = round((closedTrades["current_ask"] * closedTrades["volume"]) - (closedTrades["open_price"] * closedTrades["volume"]), 2)
+                closedTrades.insert(19, "open_date", 0)
+                closedTrades["open_date"] = pd.to_datetime(closedTrades["open_time"], unit='ms')
+                closedTrades.insert(20, "close_date", 0)
+                closedTrades["close_date"] = pd.to_datetime(closedTrades["close_time"], unit='ms')
+                closedTrades.insert(21, "current_date", 0)
+                closedTrades["current_date"] = pd.to_datetime(closedTrades["current_time"], unit='ms')
+                closedTrades.insert(22, "profit_percentage", 0)
+                closedTrades["profit_percentage"] = round((closedTrades["profit"] / (closedTrades["open_price"] * closedTrades["volume"])) * 100, 2)
+                closedTrades.insert(23, "potential_profit_percentage", 0)
+                closedTrades["potential_profit_percentage"] = round((closedTrades["potential_profit"] /(closedTrades["open_price"] * closedTrades["volume"])) * 100, 2)
 
-
-                print (closedTrades)
-
-
-                # closedTrades["after_se"]
-                #openPositions.insert(11, "sell_stop_percentage", 0.0)
-                # openPositions["sell_stop_percentage"] = -(100 - round((openPositions["sell_stop_price"] / openPositions["buy_open_price"]) * 100, 2))
-                # openPositions.insert(12, "sell_stop_value", 0.0)
-                # openPositions["sell_stop_value"] = openPositions["sell_stop_price"] * openPositions["volume"]
-                # openPositions.insert(13, "sell_stop_lost", 0.0)
-                # openPositions["sell_stop_lost"] = (openPositions["buy_open_price"] * openPositions["volume"]) - openPositions["sell_stop_value"]
-                # openPositions.insert(14, "alert_price_2", 0.0)
-                # openPositions["alert_price_2"] =  round(openPositions["buy_open_price"] * (1 + 2/100), 2)
-                # openPositions.insert(15, "alert_price_3", 0.0)
-                # openPositions["alert_price_3"] =  round(openPositions["buy_open_price"] * (1 + 3/100), 2)
-                # openPositions.insert(16, "alert_price_4", 0.0)
-                # openPositions["alert_price_4"] =  round(openPositions["buy_open_price"] * (1 + 4/100), 2)
-
-
-                # print(openPositions.sort_values("symbol",ascending=True))
-                # # OUTPUT TO FILE
-                # # now = dt.now() # current date and time
-                # # dateTimeStr = now.strftime("%Y%m%d_%H%M%S")
-                # # filename = "MyTrades_" + dateTimeStr + ".csv"
-                # #print(filename)
-                # # filename = "MyTrades.csv"
-                # # openPositions.to_csv(filename)
-
+                print (closedTrades)                   
+                # now = dt.now() # current date and time
+                # dateTimeStr = now.strftime("%Y%m%d_%H%M%S")
+                # filename = "MyTrades_" + dateTimeStr + ".csv"
+                # print(filename)
+                filename = "ClosedTrades.csv"
+                closedTrades.to_csv(filename)
             else:
-                print("Failed to get trades", response)
+                print("Failed to get trades history", response)
 
     except xapi.LoginFailed as e:
         print(f"Log in failed: {e}")
