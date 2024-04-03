f9ef1bb41cbffae2280feac31841a49d6b09d8b5
Rui Rosa Mendes <rui.rosa_mendes@siemens.com>
2024-04-04 00:09:38 +0100

    sell and buy signals

--- xtb_trading/stream_candles.py
+++ xtb_trading/stream_candles.py
@@ -14,8 +14,8 @@ with open("credentials.json", "r") as f:
 async def main():
     while True:
         try:
-            symbol = "INTERCOMP"
-            close_prices = pd.DataFrame(columns=["symbol","ctmString","open","close","high","low","vol","quoteId", "datetime", "rsiM1", "rsiM5", "rsiM15", "rsiM30"])
+            symbol = "BITCOIN"
+            close_prices = pd.DataFrame(columns=["symbol","ctmString","open","close","high","low","vol","quoteId", "datetime", "rsiM1","signal"])
             now = dt.now() # current date and time
             date_time_str = now.strftime("%Y%m%d_%H%M%S")
             filename = symbol + "_candles_" + date_time_str + ".csv"
@@ -32,23 +32,48 @@ async def main():
                     # mais regra do risco retorno (MAX 2%)  de todo o capital em risco (youtube)
                     # Sinal de compra: RSI > 30 
                     # Sinal de Venda: RSI < 70      
-                    datetime_object = dt.strptime(minute_data["ctmString"].iloc[0], '%b %d, %Y, %I:%M:%S %p')
-                    print(datetime_object)
+                    datetime_object = dt.strptime(minute_data["ctmString"].iloc[0], '%b %d, %Y, %I:%M:%S %p')                    
                     minute_data.insert(8, "datetime", datetime_object)
-
-
                     # RSI com os tick prices e ver divergências!## de ALTA ou de BAIXA!
                     # mais regra do risco retorno (MAX 2%)  de todo o capital em risco (youtube)
                     # Sinal de compra: RSI > 30 
-                    # Sinal de Venda: RSI < 70                                                        
-                    close_prices = pd.concat([close_prices, minute_data], ignore_index=True)      
-                    close_prices["rsiM1"] = ta.rsi(close_prices["close"], length=14)         
-                    close_prices["rsiM5"] = ta.rsi(close_prices["close"], length=14)         
-                    close_prices["rsiM15"] = ta.rsi(close_prices["close"], length=14)         
-                    close_prices["rsiM30"] = ta.rsi(close_prices["close"], length=14)         
-                    
-                    print(close_prices)
-                    close_prices.to_csv(filename, mode='a', header=True, index=False)
+                    # Sinal de Venda: RSI < 70      
+                    rsi_period = 7                                                                                                
+                    close_prices = pd.concat([close_prices, minute_data], ignore_index=True)                          
+                    close_prices["rsiM1"] = ta.rsi(close_prices["close"], length=rsi_period)                                             
+                    last_price = close_prices.tail(1)                                     
+                    has_signal = False                    
+                     # sell signal
+                    sell_rsi = 75
+                    filter_sell = (close_prices["rsiM1"].shift(1) < sell_rsi) & (last_price["rsiM1"] > sell_rsi)
+                    print(filter_sell.tail(rsi_period))                    
+                    if(filter_sell.tail(rsi_period).any()):                    
+                        print ("Sell signal")
+                        print ("above " + str(sell_rsi))                            
+                        print("RSI: " + str(last_price["rsiM1"].iloc[0]))
+                        print("ASK: " + str(last_price["close"].iloc[0]))                                                      
+                        close_prices.loc[close_prices.index[-1], "signal"] = "SELL"
+                        has_signal = True
+                    else:                            
+                        print ("No sell signal")
+                        close_prices.loc[close_prices.index[-1], "signal"] = "HOLD"
+
+                    # Let's check if we have a buy signal
+                    if(not has_signal):
+                        # buy signal
+                        buy_rsi = 25
+                        filter_buy = (close_prices["rsiM1"].shift(1) > buy_rsi) & (close_prices["rsiM1"].tail(1) < buy_rsi)                                        
+                        if(filter_buy.tail(rsi_period).any()):
+                            print ("Buy signal")
+                            print ("below " + str(buy_rsi))                            
+                            print("RSI: " + str(last_price["rsiM1"].iloc[0]))
+                            print("ASK: " + str(last_price["close"].iloc[0]))  
+                            close_prices.loc[close_prices.index[-1], "signal"] = "BUY"                            
+                        else:                            
+                            print ("No buy signal")                        
+                            close_prices.loc[close_prices.index[-1], "signal"] = "HOLD" 
+                    print(close_prices.tail(1))
+                    close_prices.tail(1).to_csv(filename, mode='a', header=False, index=False)
 
         except xapi.LoginFailed as e:
             print(f"Log in failed: {e}")
--- xtb_trading/stream_ticks.py
+++ xtb_trading/stream_ticks.py
@@ -9,34 +9,73 @@ import xapi
 logging.basicConfig(level=logging.INFO)
 
 with open("credentials.json", "r") as f:
-    credentials = json.load(f)    
+    credentials = json.load(f)
 
 async def main():
     while True:
         try:
-            symbol = "INTERCOMP"
-            symbol_prices = pd.DataFrame(columns=["symbol","ask","bid","low","high", "askVolume","bidVolume","spreadRaw","timestamp","datetime","rsiM1","rsiM5","rsiM15","rsiM30"])
+            symbol = "BITCOIN"
+            symbol_prices = pd.DataFrame(columns=["symbol","ask","bid","low","high", "askVolume","bidVolume","spreadRaw","timestamp","datetime","rsiM1","signal"])
+            now = dt.now() # current date and time
+            date_time_str = now.strftime("%Y%m%d_%H%M%S")
+            filename = symbol + "_ticks_" + date_time_str + ".csv"
+            print(filename)        
             async with await xapi.connect(**credentials) as x:
                 await x.stream.getTickPrices(symbol)
 
-                async for message in x.stream.listen():              
-                    data = pd.json_normalize(message['data'])                    
+                async for message in x.stream.listen():
+                    data = pd.json_normalize(message['data'])
                     tick_prices = data[["symbol","ask","bid","low","high","askVolume","bidVolume","spreadRaw", "timestamp"]]
-                    datetime = dt.fromtimestamp(tick_prices["timestamp"][0]/1000) 
+                    datetime = dt.fromtimestamp(tick_prices["timestamp"][0]/1000)
                     tick_prices.insert(9, "datetime", datetime)
+
                     # RSI com os tick prices e ver divergências!## de ALTA ou de BAIXA!
                     # mais regra do risco retorno (MAX 2%)  de todo o capital em risco (youtube)
-                    # Sinal de compra: RSI > 30 
-                    # Sinal de Venda: RSI < 70                                                        
-                    symbol_prices = pd.concat([symbol_prices, tick_prices], ignore_index=True)      
-                    symbol_prices["rsiM1"] = ta.rsi(symbol_prices["bid"], length=14)         
-                    symbol_prices["rsiM5"] = ta.rsi(symbol_prices["bid"], length=14)         
-                    symbol_prices["rsiM15"] = ta.rsi(symbol_prices["bid"], length=14)         
-                    symbol_prices["rsiM30"] = ta.rsi(symbol_prices["bid"], length=14)         
-                
-                    
-                    print(symbol_prices)
-                    
+                    # Sinal de compra: RSI > 30
+                    # Sinal de Venda: RSI < 70
+                    rsi_period = 5
+                    symbol_prices = pd.concat([symbol_prices, tick_prices], ignore_index=True)
+                    symbol_prices["rsiM1"] = ta.rsi(symbol_prices["bid"], length=rsi_period)
+                    last_price = symbol_prices.tail(1)
+        
+                    # sell signal
+                    sell_rsi = 80
+                    filter_sell = (symbol_prices["rsiM1"].shift(1) < sell_rsi) & (last_price["rsiM1"] > sell_rsi)
+                    print(filter_sell.tail(rsi_period))
+
+
+                    if(filter_sell.tail(rsi_period).any()):
+                        print ("Sell signal")
+                        print ("above " + str(sell_rsi))
+                        print("RSI: " + str(last_price["rsiM1"].iloc[0]))
+                        print("ASK: " + str(last_price["ask"].iloc[0]))
+                        symbol_prices.loc[symbol_prices.index[-1], "signal"] = "SELL"
+                    else:
+                        print ("No sell signal")
+                        # print ("below " + str(sell_rsi))
+                        # print("RSI: " + str(last_price["rsiM1"].iloc[0]))
+                        # print("ASK: " + str(last_price["ask"].iloc[0]))
+                        symbol_prices.loc[symbol_prices.index[-1], "signal"] = "HOLD"
+                    # buy signal
+                    buy_rsi = 20
+                    filter_buy = (symbol_prices["rsiM1"].shift(1) > buy_rsi) & (last_price["rsiM1"] < buy_rsi)
+                    print(filter_buy.tail(rsi_period))
+                    # last_price = symbol_prices.tail(1)
+                    if(filter_buy.tail(rsi_period).any()):
+                        print ("Buy signal")
+                        print ("below " + str(buy_rsi))
+                        print("RSI: " + str(last_price["rsiM1"].iloc[0]))
+                        print("ASK: " + str(last_price["ask"].iloc[0]))
+                        symbol_prices.loc[symbol_prices.index[-1], "signal"] = "BUY"
+                    else:
+                        print ("No buy signal")
+                        # print ("Above " + str(buy_rsi))
+                        # print("RSI: " + str(last_price["rsiM1"].iloc[0]))
+                        # print("ASK: " + str(last_price["ask"].iloc[0]))
+                        symbol_prices.loc[symbol_prices.index[-1], "signal"] = "HOLD"
+                    print(symbol_prices.tail(1))
+                    symbol_prices.tail(1).to_csv(filename, mode='a', header=False, index=False)
+
 
         except xapi.LoginFailed as e:
             print(f"Log in failed: {e}")
