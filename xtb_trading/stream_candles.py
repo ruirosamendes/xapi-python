8ae57f5159878476f1706c96cb6676888f2fa278
Rui Rosa Mendes <rui.rosa_mendes@siemens.com>
2024-05-03 17:18:04 +0100

    csv and xlsx

--- xtb_trading/stream_candles.py
+++ xtb_trading/stream_candles.py
@@ -13,9 +13,9 @@ with open("credentials.json", "r") as f:
     credentials = json.load(f)    
 
 async def main():
-    symbol_str = "RHM.DE_9"
+    #symbol_str = "RHM.DE_9"
     #symbol_str = "MSF.DE_9"            
-    # symbol_str = "PLTR.US_9"
+    symbol_str = "PLTR.US_9"
     #symbol_str = "SMCI.US_9"
     #symbol_str = "BITCOINCASH"
     now = dt.now() # current date and time
@@ -37,7 +37,7 @@ async def main():
                     datetime_object = dt.strptime(minute_data["ctmString"].iloc[0], '%b %d, %Y, %I:%M:%S %p')                    
                     minute_data.insert(8, "datetime", datetime_object)          
                     close_prices = pd.concat([close_prices, minute_data], ignore_index=True)                                    
-                    set_rsi(close_prices,"close", 14, 74, 26)
+                    set_rsi(close_prices,"close", 11, 74, 26)
                     print(close_prices.tail(1))
                     close_prices.tail(1).to_csv(filename, mode='a', header=False, index=False)
 
